c8ba85c33481444a21202aa837e3f7b3cce62f1a
ArtursMiropolecs <124880296+ArtursMiropolecs@users.noreply.github.com>
2023-03-05 19:00:12 +0000

    ArtūrsMiropolecs_221RDB388_2uzd

--- tree_height.py
+++ tree_height.py
@@ -3,33 +3,34 @@ import threading
 
 
 def compute_height(n, parents):
+    
+    root = None
     tree = {}
     for i in range(n):
         tree[i] = []
 
     for i in range(n):
-        parent = parents[i]
-        if parent == -1:
+        if parents[i] == -1:
             root = i
         else:
-            tree[parent].append(i)
+            tree[parents[i]].append(i)
 
-    def progres(node):
-        if not tree[node]:
-            return 1
+    def progress(Node):
+        
+        height = 1
+        if not tree[Node]:
+            return height
         else:
-            max_height = 0
-            for child in tree[node]:
-                max_height = max(max_height, progres(child))
-            return max_height + 1
-    return progres(root)
+            for child in tree[Node]:
+                height = max(height, progress(child))
+            
+            return height + 1
+    return progress(root)
 
 
 def main():
-    print("Enter I or F:")
-   
-
-    input_type = input().strip().lower()
+    # take input
+    input_type = input("Enter I for manual input or F for file input: ").strip().lower()
     if "i" in input_type:
         n = int(input("Enter the number of nodes: "))
         parents = list(map(int, input("Enter the parents of each node: ").split()))
@@ -54,13 +55,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
-
-
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+   
+    sys.setrecursionlimit(10**7)
+    threading.stack_size(2**27)
+    threading.Thread(target=main).start()
